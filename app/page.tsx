6b641d0ba6bc4a8f609dce18758cdf220a824940
Sebastianmoldrup <sebastian.moldrup@gmail.com>
2024-02-07 14:02:32 +0100

    barcode wip

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,10 @@
 import { createClient } from '@/utils/supabase';
 import { useState, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
-import Image from 'next/image';
 import Navigation from '@/components/navigation';
-import QRCode from 'qrcode';
 
 export default function Home() {
-  const [qrCode, setQrCode] = useState('');
+  const [barcode, setBarcode] = useState('');
   const supabase = createClient();
   const router = useRouter();
 
@@ -19,37 +17,27 @@ export default function Home() {
       // console.log(data);
       // console.log(error);
 
+      async function loadSession() {
+        const { data, error } = await supabase.auth.getSession();
+        if (data) {
+          setBarcode(data.session?.user.id ?? '');
+        } else if (error) {
+          console.error(error);
+        }
+      }
+      loadSession();
+
       if (error) router.push('/login');
     })();
-  }, [router]);
-
-  async function loadSession() {
-    const { data, error } = await supabase.auth.getSession();
-    if (data) {
-      generateQR(data.session?.user.id ?? '');
-    } else if (error) {
-      console.error(error);
-    }
-  }
-  loadSession();
+  }, [router, barcode]);
 
   const signOut = async () => {
     const { error } = await supabase.auth.signOut();
   };
 
-  const generateQR = async (text: string) => {
-    try {
-      const qrimage = await QRCode.toDataURL(text);
-      if (qrimage) setQrCode(qrimage);
-    } catch (err) {
-      console.error(err);
-    }
-  };
-
   return (
     <main className='flex flex-col items-center space-y-20 min-h-screen main-bg text-white'>
       <Navigation />
-      <Image src={qrCode} alt='qr code' width={100} height={100} />
     </main>
   );
 }
--- components/navigation.tsx
+++ components/navigation.tsx
@@ -10,8 +10,58 @@ import {
 } from '@/components/ui/drawer';
 import { Button } from '@/components/ui/button';
 import { HamburgerIcon } from '@/components/icons';
+import Image from 'next/image';
+import { useState, useEffect } from 'react';
+import { createClient } from '@/utils/supabase';
+import bwipjs from 'bwip-js';
 
 export default function Navigation() {
+  const supabase = createClient();
+  const [barcode, setBarcode] = useState('');
+  console.log(barcode);
+
+  (async () => {
+    // const supabase = createClient();
+
+    async function loadSession() {
+      const { data, error } = await supabase.auth.getSession();
+      if (data) {
+        setBarcode(data.session?.user.id ?? '');
+      } else if (error) {
+        console.error(error);
+      }
+    }
+    loadSession();
+  })();
+
+  let svg;
+  async function loadSession() {
+    const { data, error } = await supabase.auth.getSession();
+    if (data) {
+      setBarcode(data.session?.user.id ?? '');
+      barcode
+        ? (svg = bwipjs.toSVG({
+            bcid: 'code128',
+            text: barcode,
+            height: 15,
+            textxalign: 'center',
+            textcolor: 'ff0000',
+          }))
+        : null;
+    } else if (error) {
+      console.error(error);
+    }
+  }
+  loadSession();
+
+  // let svg = bwipjs.toSVG({
+  //   bcid: 'code128',
+  //   text: barcode,
+  //   height: 15,
+  //   textxalign: 'center',
+  //   textcolor: 'ff0000',
+  // });
+
   return (
     <nav className='flex justify-between px-4 py-2 w-full'>
       <h1 className='text-md font-bold'>Norsk pantelotteri</h1>
@@ -24,6 +74,10 @@ export default function Navigation() {
         <DrawerContent>
           <DrawerHeader>
             <DrawerTitle>Din strekkode</DrawerTitle>
+            <div className='flex flex-col justify-center'>
+              {/* <div dangerouslySetInnerHTML={{ __html: svg }} /> */}
+              {svg}
+            </div>
             <DrawerDescription>
               Bruk denne for å registrere glass og metal avfall
             </DrawerDescription>
